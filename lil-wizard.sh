c42145fa42b0b6e019aee6da94d319c7119b7e99
Alexander Sk <stazeworks@gmail.com>
2018-12-26 19:43:33 +0500

    Update lil-wizard.sh

--- lil-wizard.sh
+++ lil-wizard.sh
@@ -4,18 +4,18 @@
 
 lspci | grep -e VGA -e 3D
 
-echo "Choose video drivers vendor: None - 0; Intel - 1; AMD - 2; Nvidia - 3: "
+echo "Choose video drivers vendor: Intel - 1; AMD - 2; Nvidia - 3; Any other char - Skip: "
 read video_driver
 echo "Choosed option: $video_driver"
 
-if [ $video_driver = 0 ]
-    then echo "Okay, continue without Video driver..."
-elif [ $video_driver = 1 ]
+if [ $video_driver = 1 ]
   then pacman -S xf86-video-intel
 elif [ $video_driver = 2 ]
   then pacman -S xf86-video-amdgpu
-else [ $video_driver = 3 ]
-  then pacman -S nvidia
+elif [ $video_driver = 3 ]
+    then pacman -S nvidia
+else
+  echo "Okay, continue without Video driver..."
 fi
 
 sudo pacman -S xorg-server xorg-init i3-gaps i3status rxvt-unicode dmenu ttf-linux-libertine ttf-inconsolata
