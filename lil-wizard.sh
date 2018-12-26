a33d6085a7fde201376fb3c2a813314f490e5318
Alexander \'Staze' Sk <stazeworks@gmail.com>
2018-12-26 19:39:57 +0500

    upd

--- lil-wizard.sh
+++ lil-wizard.sh
@@ -10,11 +10,11 @@ echo "Choosed option: $video_driver"
 
 if [ $video_driver = 0 ]
     then echo "Okay, continue without Video driver..."
-if [ $video_driver = 1 ]
+elif [ $video_driver = 1 ]
   then pacman -S xf86-video-intel
-if [ $video_driver = 2 ]
+elif [ $video_driver = 2 ]
   then pacman -S xf86-video-amdgpu
-if [ $video_driver = 3 ]
+else [ $video_driver = 3 ]
   then pacman -S nvidia
 fi
 
