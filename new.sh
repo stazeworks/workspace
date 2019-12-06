5ad2254ba1a4ad4008fde8a54e2f55770c81bf79
Alexander Sk <stazeworks@gmail.com>
2019-12-06 08:15:03 +0500

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
+# FUCNTIONS
+
+error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
+
+checkBios() { \
+	if [ -d ls /sys/firmware/efi/efivars ]; then
+    echo "Fine. We are on UEFI install, keep going."
+  else
+    error "No way, man. UEFI ONLY!"
+  fi
+  }
+
+
 # CHECKS
 checkBios
-
 
 
 # PARTITION
@@ -19,14 +31,4 @@ timedatectl set-ntp true
 
 
 
-# FUCNTIONS
 
-error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
-
-checkBios() { \
-	if [ -d ls /sys/firmware/efi/efivars ]; then
-    echo "Fine. We are on UEFI install, keep going."
-  else
-    error "No way, man. UEFI ONLY!"
-  fi
-  }
