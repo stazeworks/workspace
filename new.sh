9ef6ddd59436d3caf317be85333f8989d05c3111
Alexander Sk <stazeworks@gmail.com>
2019-12-06 09:06:51 +0500

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
-echo "ALLO"
+
+echo "HELLO"
+
 # VARS
 
-
 if [ -z "$target" ]; then
-read -p "Installation disk is: (sdX)" value
-echo "Install system into: $target"
+	read -p "Installation disk is: (sdX)" target
+	echo "Install system into: $target"
 else
-echo "Install system into: $target"
+	echo "Install system into: $target"
 fi
+
+
 # FUCNTIONS
 
 error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
 
 checkBios() { \
 if [ -d /sys/firmware/efi/efivars ]; then
-echo "Fine. We are on UEFI install, keep going."
+	echo "Fine. We are on UEFI install, keep going."
 else
-error "No way, man. UEFI ONLY!"
+	error "No way, man. UEFI ONLY!"
 fi
 }
 
 
 # CHECKS
-#checkBios
+checkBios
 
 
 # PARTITION
-#parted /dev/sda mklabel gpt mkpart P1 ext3 1MiB 8MiB 
+parted /dev/$target mklabel gpt mkpart P1 ext3 1MiB 8MiB 
 
 
 # INSTALL PROCESS
