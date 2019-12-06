93cd5a4ab9d0152fa541324dca15a894c0741144
Alexander Sk <stazeworks@gmail.com>
2019-12-06 19:26:59 +0500

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # VARS
-dateLog=$(date --rfc-3339='date')
+dateLog=$(date --rfc-3339='seconds')
 
 if [ -z "$target" ]; then
-	read -p "Which > sdX < we'll be use to install our workspase? " target
+	read -p "Which DISK we'll be use to install our workspase? (sdX)" target
 	log OK "Install system into: $target"
 else
 	log DONE "Install system into: $target"
@@ -19,7 +19,7 @@ error() { clear; printf "[*] [$dateLog] [ERROR]:\\n%s\\n" "$1"; exit;}
 
 checkBios() {
 	if [ -d /sys/firmware/efi/efivars ]; then
-		log LOG "[*] [OK] UEFI detected."
+		log OK "UEFI detected."
 	else
 		error "It\'s BIVOS, not UEFI!"
 	fi
@@ -28,13 +28,13 @@ checkBios() {
 partiotion() {
 	log INFO "Partiotions\\n   [#] ---- Before: ---- [#]"
 	lsblk
-	echo -e "[#] ------------------- [#]\n"
+	echo -e "[#] ------------------- [#]"
 	parted /dev/$target --script mklabel gpt \
 		mkpart primary fat32 1MiB 300MiB \
 		set 1 esp on \
 		mkpart primary ext2 300MiB 700MiB \
 		mkpart primary ext4 700MiB 100%
-	log INFO "Partiotions\\n   [#] ---- After: ---- [#]"
+	log INFO "   [#] ---- After: ---- [#]"
 	lsblk
 	echo -e "[#] ------------------- [#]\n"
 }
