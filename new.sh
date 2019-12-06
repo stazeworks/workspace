07d76845fb81f08cf0a7b3e57d1300dab30a2aad
Alexander Sk <stazeworks@gmail.com>
2019-12-06 09:27:20 +0500

    Update new.sh

--- new.sh
+++ new.sh
@@ -18,7 +18,7 @@ error() { clear; printf "ERROR:\\n%s\\n" "$1"; exit;}
 
 checkBios() { \
 if [ -d /sys/firmware/efi/efivars ]; then
-	echo "Fine. We are on UEFI install, keep going."
+	echo "[*] UEFI detected."
 else
 	error "No way, man. UEFI ONLY!"
 fi
@@ -30,11 +30,16 @@ checkBios
 
 
 # PARTITION
+echo "[*] Before parted:"
+lsblk
+echo "[*] -------------------"
 parted /dev/$target --script mklabel gpt mkpart primary fat32 1MiB 300MiB 
 parted /dev/$target --script set 1 esp on
 parted /dev/$target --script mkpart primary ext2 300MiB 700MiB
 parted /dev/$target --script mkpart primary ext4 700MiB 100%
+echo "[*] After parted:"
 lsblk
+echo "[*] -------------------"
 # INSTALL PROCESS
 
 loadkeys ru
