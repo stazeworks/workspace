3b5e0350a6603118f9755c2ebf41620b12c3f562
Alexander Skoromnyuk <askoromnyuk@stream.ru>
2019-12-11 19:07:39 +0300

    update

--- new.sh
+++ new.sh
@@ -3,12 +3,7 @@
 # VARS
 dateLog=$(date --rfc-3339='seconds')
 
-if [ -z "$target" ]; then
-	read -p "Which DISK we'll be use to install our workspase? (sdX) " target
-	log OK "Install system into: $target"
-else
-	log DONE "Install system into: $target"
-fi
+
 
 # FUCNTIONS
 
@@ -16,12 +11,25 @@ log() { echo -e "[*] [$dateLog] [$1] $2"; }
 
 error() { clear; printf "[*] [$dateLog] [ERROR]:\\n%s\\n" "$1"; exit;}
 
-checkBios() {
+selectDisk() {
+	if [ -z "$target" ]; then
+		read -p "Which DISK we'll be use to install our workspase? (sdX) " target
+		log OK "Install system into: $target"
+	else
+		log DONE "Install system into: $target"
+	fi
+}
+
+preinstall() {
 	if [ -d /sys/firmware/efi/efivars ]; then
 		log OK "UEFI detected."
 	else
 		error "It\'s BIVOS, not UEFI!"
 	fi
+
+	timedatectl set-ntp true
+	timedatectl set-timezone Europe/Moscow
+	timedatectl status	
 }
 
 partiotion() {
@@ -29,20 +37,24 @@ partiotion() {
 	echo -e "\n   [#] ---- BEFORE: ---- [#]"
 	lsblk
 	echo -e "[#] ------------------- [#]"
-	parted /dev/$target --script mklabel gpt \
-		mkpart primary fat32 1MiB 300MiB \
+
+	parted -s /dev/sda mklabel gpt \
+		mkpart efi '0%' '512MB' \
+		mkpart crypt 513MB '100%' \
 		set 1 esp on \
-		mkpart primary ext2 300MiB 700MiB \
-		mkpart primary ext4 700MiB 100%
+		set 1 boot on print && \
+
 	echo -e "\n   [#] ---- AFTER: ---- [#]"
 	lsblk
 	echo -e "[#] ------------------- [#]\n"
 }
 
 
+
 # RUN IN ORDER
 
 log START "Let battle begins!"
+selectDisk
 checkBios
 partiotion
 
@@ -52,9 +64,9 @@ partiotion
 loadkeys ru
 setfont cyr-sun16
 timedatectl set-ntp true
-timedatectl status
 
 
+#arch-chroot /mnt sh -c "$(curl -fsSL https://git.io/X220b.sh)"
 
 
 
