2eb1fa575322e587f374e387e0435a3c8b684dda
Alexander 'Staze' Skoromnyuk <stazeworks@gmail.com>
2021-02-13 22:40:54 +0300

    update

--- x220.sh
+++ x220.sh
@@ -21,6 +21,7 @@ preinstall() {
 		log INFO "[BIOS] / uefi"
 	fi
 
+	log INFO "Time&date setup:\n"
 	timedatectl set-ntp true
 	timedatectl set-timezone Europe/Moscow
 	timedatectl status	
@@ -54,4 +55,47 @@ ping ntp.org -c 3
 loadkeys ru
 setfont cyr-sun16
 timedatectl set-ntp true
-log INFO Done!
+
+log INFO "LUKS setup:\n"
+cryptsetup luksFormat /dev/sda2
+cryptsetup open /dev/sda2 cryptlvm
+
+vgcreate vg0 /dev/mapper/cryptlvm
+lvcreate -L 50G vg0 -n root
+lvcreate -L 16G vg0 -n swap
+lvcreate -L 157G vg0 -n home
+
+modprobe dm_mod
+vgscan
+vgchange -ay
+
+lsblk /dev/sda
+
+
+log INFO "\n Format partitions:\n"
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/vg0/root
+mkfs.ext4 /dev/vg0/home
+
+mkswap /dev/vg0/swap
+swapon /dev/vg0/swap
+
+mount /dev/vg0/root /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+mkdir /mnt/home
+mount /dev/vg0/home /mnt/home
+log INFO "Disk mount is over"
+
+
+log INFO "Set hardcoded mirror (yandex)"
+Server = https://mirror.yandex.ru/archlinux/$repo/os/$arch > /etc/pacman.d/mirrorlist
+
+log INFO "\nInstalling system. Setup Pacman:\n"
+pacstrap /mnt base linux linux-firmware vim git intel-ucode efibootmgr openssh wpa_supplicant dhcpcd cryptsetup lvm2
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+# arch-chroot /mnt bash
+
+log INFO "Chroot into installed Archlinux"
