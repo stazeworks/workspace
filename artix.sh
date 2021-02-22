23e556c00cf7852d00358d4eea29d886557f24cf
Alexander 'Staze' Skoromnyuk <stazeworks@gmail.com>
2021-02-22 15:56:31 +0300

    update

--- artix.sh
+++ artix.sh
@@ -1,36 +1,114 @@
-#!/bin/sh
+sudo su
 
-TARGET_DISK="null"
-WIPEOUT_TARGET_DISK=0
+################################################
+# Preinstall: Wifi                             #
+################################################
 
-log() { echo -e "[*] [$dateLog] [$1] $2"; }
+# 1. Interfaces list:
+#ip a
 
-selectDisk() {
-	if [ -z "$TARGET_DISK" ]; then
-        lsblk
-		read -p "Select disk for installation. Current disk is - ${TARGET_DISK}" $TARGET_DISK
-		log INFO "Install system into: $TARGET_DISK"
-	else
-		log CRITICAL "You must select a disk!"
-        selectDisk          
-	fi
-}
+# 2. Find wifi interface, f.e. wlan0
+#sudo ip link set wlan0 up
 
-wipeoutDisk() {
-    log WARN "W A R N I N G: Wipeout all data in ${TARGET_DISK}"
-    read -p "0 - NO, 1 - YEZ: " $WIPEOUT_TARGET_DISK
-    if [[ $WIPEOUT_TARGET_DISK == 0 ]]; then
-	    log INFO "Skipping the wipeout"
-    elif [[ $WIPEOUT_TARGET_DISK == 1 ]]; then
-        log INFO "Wipeout all data in ${TARGET_DISK}"
-	    #wipefs -a /dev/sda
-    fi
-}
+# 2.1 Problems? Try this:
+#sudo rfkill unblock wifi
+#sudo ip link set wlan0 up
 
-preparePartiotions() {
-    log INFO "Test"
-}
+# 3. Start setup wifi connection
+#connmanctl
+#> scan wifi
+#> services
+# Choose stroke this your SSID
+#> agent on
+# Use TAB for matching your SSID
+#> connect wifi_xxxxxxx_xxxxxx...
+# Passphrase:
+#> blabla
+#> quit
 
-selectDisk
-wipeoutDisk
-preparePartiotions
+# 4. Checks:
+ip a
+ping ntp.org -c 3
+
+
+
+################################################
+# Preinstall: Tools                            #
+################################################
+
+# 5. Install tools: internet is required!
+pacman -Sy openssh-runit parted --noconfirm
+ln -s /etc/runit/sv/sshd /run/runit/service/sshd
+sv start sshd
+
+
+
+################################################
+# Preinstall: Time and Date                    #
+################################################
+
+timedatectl set-ntp true
+timedatectl set-timezone Europe/Moscow
+timedatectl status
+
+
+
+################################################
+# Preinstall: Console output                   #
+################################################
+loadkeys ru
+setfont cyr-sun16
+
+
+
+################################################
+# Preinstall: Partiotions                      #
+################################################
+
+# 6. UEFI or BIOS?
+ls /sys/firmware/efi/efivars
+
+# 7. Wipeout target disk
+lsblk
+# Wipeout LVM?
+#pvremove -y -ff /dev/sda*
+
+sudo parted -s /dev/sda mklabel gpt mkpart efi '0%' '512MB' mkpart crypt 513MB '100%' set 1 esp on set 1 boot on print
+
+cryptsetup luksFormat /dev/sda2
+cryptsetup open /dev/sda2 cryptlvm
+
+vgscan
+vgchange -ay
+
+vgcreate vg0 /dev/mapper/cryptlvm
+lvcreate -L 30G vg0 -n root
+lvcreate -L 16G vg0 -n swap
+lvcreate -L 177G vg0 -n home
+
+modprobe dm_mod
+
+lsblk /dev/sda
+
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/vg0/root
+mkfs.ext4 /dev/vg0/home
+
+mkswap /dev/vg0/swap
+swapon /dev/vg0/swap
+
+mount /dev/vg0/root /mnt
+mkdir /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+mkdir /mnt/home
+mount /dev/vg0/home /mnt/home
+
+
+################################################
+# Preinstall: Installation                     #
+################################################
+
+basestrap /mnt base base-devel runit elogind-runit linux linux-firmware vim git dhcpcd-runit
+
+fstabgen -U /mnt >> /mnt/etc/fstab
